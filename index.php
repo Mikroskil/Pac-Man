0eba3bb6d79bf7063b340edeae9f4d7433972e19
Posman Juniadi Silalahi <1989JunPo@users.noreply.github.com>
2013-11-06 14:37:55 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>allcompany_portal</title>
+<style type="text/css">
+<!--
+body {
+	background-color: #000000;
+}
+body,td,th {
+	color: #FFFFFF;
+}
+.style1 {
+	font-size: 24px;
+	font-family: "Adobe Garamond Pro", Garamond, Georgia, sans-serif;
+}
+-->
+</style></head>
+
+<body>
+<table width="800" border="1" align="center">
+  <tr>
+    <th colspan="4" scope="col"><img src="image/home.jpg" width="1286" height="873" border="0" usemap="#Map" /></th>
+  </tr>
+  <tr>
+    <td height="316" colspan="2" align="center" valign="top"><p>Deskripsi keuntungan join di portal web ini :</p>
+    <p>- Anda akan mendapat kemudahan dalam maintenance web.</p>
+    <p>- Pengelolaan web perusahaan anda menjadi lebih mudah.</p>
+    <p>- Terdapat banyak fitur yang memberi keleluasaan dalam pengaturan web. </p></td>
+    <td width="372">&nbsp;</td>
+    <td width="350">&nbsp;</td>
+  </tr>
+  <tr>
+    <td width="246"><div align="center" class="style1"><img src="image/join.jpg" width="212" height="34" border="0" usemap="#Map2" /></div></td>
+    <td width="295"><div align="center"></div></td>
+    <td><div align="center"></div></td>
+    <td><div align="center"></div></td>
+  </tr>
+  <tr>
+    <td colspan="4"><img src="image/footer web.png" width="1281" height="178" /></td>
+  </tr>
+</table>
+
+<map name="Map" id="Map">
+<area shape="rect" coords="37,794,193,842" href="sites/medan.php" alt="Medan" />
+<area shape="rect" coords="226,802,424,847" href="sites/jakarta.php" alt="jakarta" />
+<area shape="rect" coords="449,795,653,844" href="sites/bandung.php" alt="bandung" />
+<area shape="rect" coords="686,798,911,844" href="sites/surabaya.php" alt="surabaya" />
+<area shape="rect" coords="948,796,1233,842" href="sites/yogya.php" alt="jogja" />
+</map>
+<map name="Map2" id="Map2"><area shape="rect" coords="25,4,188,33" href="#" target="_blank" alt="belum di link" />
+</map></body>
+</html>
