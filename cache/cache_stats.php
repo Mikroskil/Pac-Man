f32b5ed74e01c445d315e7dd8493bf0e8b73f978
Stefdarz <Stefdarz@yahoo.co.id>
2014-01-14 23:19:53 +0700

    Create cache_stats

---
+++ cache/cache_stats.php
@@ -0,0 +1,17 @@
+<?php
+
+if (!defined('FORUM_STATS_LOADED')) define('FORUM_STATS_LOADED', 1);
+
+$forum_stats = array (
+  'total_users' => '2',
+  'last_user' => 
+  array (
+    'id' => '3',
+    'username' => 'HelloWorld',
+  ),
+  'total_topics' => '2',
+  'total_posts' => '3',
+  'cached' => 1388752806,
+);
+
+?>
