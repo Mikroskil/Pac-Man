074ac566f4605fe94b370686af08a7f702cd62aa
Darwin <landchuckz@yahoo.com>
2014-01-18 21:53:24 +0700

    Update and rename extensions to forum/admin/extensions.php

--- forum/admin/extensions.php
+++ forum/admin/extensions.php
@@ -1,14 +1,4 @@
 <?php
-/**
- * Extension and hotfix management page.
- *
- * Allows administrators to control the extensions and hotfixes installed in the site.
- *
- * @copyright (C) 2008-2012 PunBB, partially based on code (C) 2008-2009 FluxBB.org
- * @license http://www.gnu.org/licenses/gpl.html GPL version 2 or higher
- * @package PunBB
- */
-
 
 if (!defined('FORUM_ROOT'))
 	define('FORUM_ROOT', '../');
