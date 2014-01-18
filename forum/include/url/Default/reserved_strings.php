43d64f8ea98b20b52b8d46c89b2a2d86a169a137
Darwin <landchuckz@yahoo.com>
2014-01-18 22:34:07 +0700

    Create reserved_strings.php

---
+++ forum/include/url/Default/reserved_strings.php
@@ -0,0 +1,15 @@
+<?php
+
+$forum_reserved_strings = array(
+	''				=>	'view',
+
+	'newpost'		=>	'view',
+	'newposts'		=>	'view',
+	'new-post'		=>	'view',
+	'new-posts'		=>	'view',
+
+	'lastpost'		=>	'view',
+	'lastposts'		=>	'view',
+	'last-post'		=>	'view',
+	'last-posts'	=>	'view',
+);
